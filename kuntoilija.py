c3ce67150d61841a900be4532fe7e81c3114fb95
Dildora <48212@edu.raseko.fi>
2023-02-03 10:33:53 +0200

    Aloitettu periytimisharjoitus

--- kuntoilija.py
+++ kuntoilija.py
@@ -1,16 +1,18 @@
-#KUNTOJLIJAN TIEDOT OLIO OHJELMOITINA
+#KUNTOJLIJAN TIEDOT OLIO OHJELMOINTINA
 #====================================
 
-#kIRJASTOT JA MODULIT (LIBRARIES AND MODULES)
+#KIRJASTOT JA MODULIT (LIBRARIES AND MODULES)
 #--------------------------------------------
 
 import fitnes
 
-#LUKKAMÄÄRÄYKSET (CLASS DEFINITION)
+#LUOKKAMÄÄRÄYKSET (CLASS DEFINITION)
 
 
 class Kuntoilija:
     """Luokka kuntoilijan tietoa varten"""
+
+    #Olimuodostin eli konstruktori
     def __init__(self, nimi, pituus, paino, ika, sukupuoli):
 
         #MÄÄRITELLÄÄN TULEVAN OLION OMINAISUUDET (PROPERTY), LUOKAN KENTÄÄT (FIELD)
@@ -19,11 +21,21 @@ class Kuntoilija:
         self.paino = paino
         self.ika = ika
         self.sukupuoli = sukupuoli
+        self.bmi = fitnes.laske_bmi(self.paino, self.pituus)
+        
+
+    #Metodi rasvaprosentin laskimeseen (aikuinen)
+    def rasvaprosentti (self):
+        self.rasvaprosentti = fitnes.aikuisen_rasvaprosentti(self.bmi, self.ika, self.sukupuoli)
+        return self.rasvaprosentti 
+
+
 
 if __name__ == "__main__":
             
     #Luodaan olio luokasta Kuntoilija
     kuntoilija = Kuntoilija("Kalle Kuntoilija", 171, 65, 40, 1)
     print(kuntoilija.nimi, "painaa",  kuntoilija.paino, "kg")
-
+    print('painoindeksi on', kuntoilija.painoindeksi())
+    print('painoindeksi on', kuntoilija.rasvaprosentti())
         
