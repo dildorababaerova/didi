1bd1cf079017c373468f1a38907359e05f274888
Dildora <48212@edu.raseko.fi>
2023-02-03 11:14:27 +0200

    lisätty aliluokka

--- kuntoilija.py
+++ kuntoilija.py
@@ -8,7 +8,7 @@ import fitnes
 
 #LUOKKAMÄÄRÄYKSET (CLASS DEFINITION)
 
-
+# Kuntoilija - luokka Yliluokka Juniorikuntoilijalle (super class)
 class Kuntoilija:
     """Luokka kuntoilijan tietoa varten"""
 
@@ -24,16 +24,33 @@ class Kuntoilija:
         self.bmi = fitnes.laske_bmi(self.paino, self.pituus)
         
 
-    #Metodi rasvaprosentin laskimeseen (aikuinen)
+    # Metodi rasvaprosentin laskimeseen (aikuinen / yleinen)
     def rasvaprosentti (self):
         self.rasvaprosentti = fitnes.aikuisen_rasvaprosentti(self.bmi, self.ika, self.sukupuoli)
         return self.rasvaprosentti 
 
 
+# JunioriKuntoilija_luokka Kuntoilija-luokan aliluokka(subclass)
+class JunioriKuntoilija(Kuntoilija):
+    """Luokka nuoren kuntoilijan tiedoille"""
+    def __init__(self, nimi, pituus, paino, ika, sukupuoli):
+
+        # Määritellään periytyminen, mitä ominaisuuksia perii kun luokan sisälle aliluokka
+        super().__init__(nimi, pituus, paino, ika, sukupuoli)
+        
+    # Metodi rasvaprosentin laskimeseen (ylikirjoitettu lapsen metodilla)
+    def rasvaprosentti (self):
+        self.rasvaprosentti = fitnes.lapsen_rasvaprosentti(self.bmi, self.ika, self.sukupuoli)
+        return self.rasvaprosentti 
+
+    
+    
+
+
 
 if __name__ == "__main__":
             
-    #Luodaan olio luokasta Kuntoilija
+    # Luodaan olio luokasta Kuntoilija
     kuntoilija = Kuntoilija("Kalle Kuntoilija", 171, 65, 40, 1)
     print(kuntoilija.nimi, "painaa",  kuntoilija.paino, "kg")
     print('painoindeksi on', kuntoilija.painoindeksi())
