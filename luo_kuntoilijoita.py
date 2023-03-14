540e0a18cb468c039e1b94cf18aea3f42ef318ad
Dildora <48212@edu.raseko.fi>
2023-03-14 12:35:57 +0200

    lisä
    lisä

--- luo_kuntoilijoita.py
+++ luo_kuntoilijoita.py
@@ -1,41 +1,46 @@
 # GET BASIC INFORMATION ABOUT AN ATHLETE AND CREATE ATHLETE OBJECTS
 # -------------------------------------------------------
+
 # LIBRARIES AND MODULES
 import kuntoilija
 import questions
+
+
 # Ask a question and convert the answer to float
+
+
 # Enter information about an athlete
 name = input('Nimi: ')
 
 # Ask details about her/him
-question = questions.Question('Kuinka paljon painat (kg): ')
-weight = question.ask_user_float(True)[0]
-question = questions.Question('Kuinka pitkä olet (cm): ')
-height = question.ask_user_float(True)[0]
-question = questions.Question('Kuinka vanha olet: ')
-age = question.ask_user_integer(True)[0]
-question = questions.Question('Sukupuoli 1 mies, 0 nainen: ')
-gender = question.ask_user_integer(True)[0]
-question = questions.Question('Mikä on kaulanympäryksesi (cm): ')
-neck = question.ask_user_float(True)[0]
-question = questions.Question('Mikä on vyötärönympäryksesi: ')
-waist = question.ask_user_float(True)[0]
+
+weight = questions.Question.ask_user_float('Kuinka paljon painat (kg): ', True)[0]
+height = questions.Question.ask_user_float('Kuinka pitkä olet (cm): ', True)[0]
+age = questions.Question.ask_user_integer('Kuinka vanha olet', True)[0]
+gender = questions.Question.ask_user_integer('Sukupuoli 1 mies, 0 nainen: ', True)[0]
+neck = questions.Question.ask_user_float('Mikä on kaulanympäryksesi (cm): ', True)[0]
+waist = questions.Question.ask_user_float('Mikä on vyötärönympäryksesi: ', True)[0]
+
+# If woman ask circumference of her hips
 if gender == 0:
-    question = questions.Question('Mikä on lantionympäryksesi: ')
-    hips = question.ask_user_float(True)[0]
+    hips = questions.Question.ask_user_float('Mikä on lantionympäryksesi: ', True)[0]
 
 # Create an athelete object from Kuntoilija class
 athlete = kuntoilija.Kuntoilija(name, height, weight, age, gender)
+
 # Print some information about the athlete
 text_to_show = f'Terve {athlete.nimi}, painoindeksisi tänään on {athlete.bmi}'
 print(text_to_show)
 fat_percentage = athlete.rasvaprosentti()
+
+# If male use usa_rasvaprosentti_mies method
 if gender == 1:
     usa_fat_percentage = athlete.usa_rasvaprosentti_mies(height, waist, neck)
 else:
     usa_fat_percentage = athlete.usa_rasvaprosentti_nainen(
         height, waist, hips, neck)
+
 text_to_show = f'suomalainen rasva-% on {fat_percentage} ja amerikkalainen on {usa_fat_percentage}'
 print(text_to_show)
 
-#TODO: Save a user information to a file
+# TODO: Save user information to a file
